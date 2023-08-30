b125a8d20ea81c0164ad51d544dc774e77196c00
Kamyar Maham <kamyar_maham1381@yahoo.com>
2023-08-30 17:02:25 +0330

    insert some data into users_with_access_levels

---
+++ insert_data_to_users_with_access_levels.sql
@@ -0,0 +1,7 @@
+use quera;
+
+
+INSERT INTO users_with_access_levels (level, name, family, birth_date)
+VALUES
+       (DEFAULT, 'abolfazl', 'mohajeri', '1991-08-07 00:00:00'),
+       ('admin', 'nima', 'heydarinasab', '1989-03-22 00:00:00');
