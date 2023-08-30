841a847aa72e03c4aeee0caf130d6204c2bd6b4e
Kamyar Maham <kamyar_maham1381@yahoo.com>
2023-08-30 19:58:41 +0330

    practiced deleting those records i mentioned before

---
+++ practice_delete_some_records_from_users_with_salary.sql
@@ -0,0 +1,10 @@
+-- Section1
+delete from users_with_salary
+    where family like '%m%' or family like '%d%';
+-- Section2
+delete from users_with_salary
+    where family = 'mohammadi' or salary in (7356, 9701, 2885, 7414, 3801);
+
+-- Section3
+delete from users_with_salary
+    where birth_date between '1995-01-01' and '2000-12-31' or family = 'booazar';
