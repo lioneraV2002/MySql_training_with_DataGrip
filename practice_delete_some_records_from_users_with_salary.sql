90e5c94a00b5eb6a3c3b1c4907a3ae539d4140cf
Kamyar Maham <kamyar_maham1381@yahoo.com>
2023-08-30 20:01:20 +0330

    made a little change

--- practice_delete_some_records_from_users_with_salary.sql
+++ practice_delete_some_records_from_users_with_salary.sql
@@ -1,6 +1,6 @@
 -- Section1
 delete from users_with_salary
-    where family like '%m%' or family like '%d%';
+    where family not like '%m%' and family not like '%d%';
 -- Section2
 delete from users_with_salary
     where family = 'mohammadi' or salary in (7356, 9701, 2885, 7414, 3801);
