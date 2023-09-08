27087f268c045f918d656c44754b609fbda38d41
Kamyar Maham <kamyar_maham1381@yahoo.com>
2023-09-08 23:41:31 +0330

    create a games table and load csv file data into it

---
+++ create_table_insert_data_from_csv_games.sql
@@ -0,0 +1,24 @@
+use quera;
+create table games
+(
+    name            varchar(255),
+    platform        VARCHAR(255),
+    year_of_release YEAR,
+    genre           VARCHAR(255),
+    publisher       VARCHAR(255),
+    na_sales        DOUBLE(10, 2),
+    other_sales     DOUBLE(10, 2),
+    global_sales    DOUBLE(10, 2)
+);
+
+
+LOAD DATA LOCAL INFILE 'games.csv'
+    INTO TABLE games
+    FIELDS TERMINATED BY ',' optionally enclosed by '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES
+    (name,platform,year_of_release, genre, publisher, na_sales, other_sales, global_sales);
+
+
+show global variables like 'local_infile';
+set global local_infile = true;
